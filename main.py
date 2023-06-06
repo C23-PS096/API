e967b3e94a3ddbb9f6aa241fca7b5868d57e9232
Haffif <haffifrasyafauzi@gmail.com>
2023-06-06 23:12:02 +0700

    add kacamata

--- main.py
+++ main.py
@@ -43,5 +43,35 @@ def getFacePrediction():
                 'message': "Upload a file"
             })
 
+@app.route("/kacamata", methods=['POST', 'GET'])
+def getKacamataPrediction():
+    if request.method == "GET":
+        return jsonify({
+            'status': 200,
+            'message': "Endpoint called"
+        })
+    if request.method == "POST":
+        file_upload = request.files['file']
+        
+        if file_upload:    
+            file_contents = file_upload.read()
+            file_stream = BytesIO(file_contents)
+            
+            prediction_result = predictions(file_stream)
+            prediction_result['probability'] = float(prediction_result['probability'])
+            print(prediction_result)
+            
+            return jsonify({
+                'status': 200,
+                'message': "Success",
+                'data': prediction_result
+            })
+
+        else:
+            return jsonify({
+                'status': 400,
+                'message': "Upload a file"
+            })
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
